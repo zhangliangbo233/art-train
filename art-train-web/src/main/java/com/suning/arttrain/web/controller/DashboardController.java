6cc854072cffa8b278c1174921e0f4d974652f18
zhanglb <zhangliangbo233@163.com>
2016-07-02 22:28:46 +0800

    框架布局修改

--- art-train-web/src/main/java/com/suning/arttrain/web/controller/DashboardController.java
+++ art-train-web/src/main/java/com/suning/arttrain/web/controller/DashboardController.java
@@ -1,18 +1,19 @@
 package com.suning.arttrain.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-public class SystemManageController extends BaseController{
+@Controller
+public class DashboardController extends BaseController{
 
 	/**
 	 * 显示首页
 	 * @return
 	 */
 	@RequestMapping("/dashboard")
-	public ModelAndView index(){
+	public ModelAndView dashboard(){
 		ModelAndView view = new ModelAndView("dashboard");
 		return view;
 	}
